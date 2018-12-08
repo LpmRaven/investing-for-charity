088f1690d3f9082226d68d7fd6966edd6f82af86
IFM\JIoppolo <jioppolo@infomedia.com.au>
2018-12-08 17:22:43 +1100

    Added preventative mental health page

--- src/pages/globalPoverty.js
+++ src/pages/globalPoverty.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
 import GlobalPovertyImage1 from '../images/2018-11-24 Global Poverty INFOGRAPH 1-page-001.jpg';
+import GlobalPovertyImage2 from '../images/2018-11-24 Global Poverty INFOGRAPH 2-page-001.jpg';
 
 const GlobalPovertyPage = () => (
   <Layout>
     <h1>Global Poverty</h1>
     <img alt='global poverty page 1' src={GlobalPovertyImage1} />
+    <img alt='global poverty page 2' src={GlobalPovertyImage2} />
   </Layout>
 );
 
