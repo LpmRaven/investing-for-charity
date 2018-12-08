acc6edd99784cebf60c0de8bb31f6838decac53c
Ivan <ivan.rahn@gmail.com>
2018-12-08 17:26:31 +1100

    add charity infographics

---
+++ src/pages/womenDomesticViolence.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+import WomenDomesticViolence1 from '../images/2018-09-17 Women _ Domestic Violence INFOGRAPH-page-001.jpg';
+
+const WomenDomesticViolence = () => (
+  <Layout>
+    <h1>Women Domestic Violence</h1>
+    <img alt='women domestic violence' src={WomenDomesticViolence1} />
+  </Layout>
+);
+
+export default WomenDomesticViolence;
