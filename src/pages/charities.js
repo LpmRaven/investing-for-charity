9c3d1e894d30219b8f69ae0e839308b500d88c6f
IFM\JIoppolo <jioppolo@infomedia.com.au>
2018-12-08 17:06:05 +1100

    Added charities page

--- src/pages/charities.js
+++ src/pages/charities.js
@@ -6,6 +6,12 @@ import Layout from '../components/layout';
 const CharitiesPage = () => (
   <Layout>
     <h1>Charities</h1>
+    <Link to="/globalPoverty/">Global Poverty</Link>
+    <Link to="/preventativeMentalHealth/">Preventative Mental Health</Link>
+    <Link to="/refugeesInAustralia/">Refugees In Australia</Link>
+    <Link to="/childSlavery/">Child Slavery</Link>
+    <Link to="/underprivilegedYouth/">Underprivileged Youth</Link>
+    <Link to="/womenDomesticVioloence/">Women Domestic Violence</Link>
     <Link to="/summary/">Back to Summary</Link>
   </Layout>
 );
---
+++ src/pages/globalPoverty.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+import GlobalPovertyImage1 from '../images/2018-11-24 Global Poverty INFOGRAPH 1-page-001.jpg';
+
+const GlobalPovertyPage = () => (
+  <Layout>
+    <h1>Global Poverty</h1>
+    <img alt='global poverty page 1' src={GlobalPovertyImage1} />
+  </Layout>
+);
+
+export default GlobalPovertyPage;
