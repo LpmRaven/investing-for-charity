d4bcf701d3ac98470b9c27cb9bb4ad45ee750b7f
andiwinata <andi.winata@hotmail.com>
2018-12-10 09:45:20 +1100

    fix layout css

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import '../styles/styles.scss'
+import './layout.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
